0bb4e0823048b0d02201fe03ed22d6a757c82ee6
Jan Zukowski <janzukowski@proton.me>
2025-12-22 14:37:28 +0100

    update: added roulette controllers

---
+++ RouletteApi/Controllers/RoulettesController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RouletteApi.Context;
+using RouletteApi.Models;
+
+namespace RouletteApi.Controllers
+{
+    public class RoulettesController : Controller
+    {
+        private readonly DatabaseContext database;
+
+        public RoulettesController(DatabaseContext context)
+        {
+            database = context;
+        }
+
+        public IResult Index()
+        {
+            var records = from roulette in database.Set<Roulette>()
+                         select new
+                         {
+                             Id = roulette.Id,
+                             Name = roulette.Name
+                         };
+
+            return Results.Ok(records);
+        }
+
+        public IResult Details(int id)
+        {
+            var records = from roulettes in database.Set<Roulette>()
+                         join items in database.Set<Item>()
+                         on roulettes.Id equals items.RouletteId into grouping
+                         where roulettes.Id == id
+                         select new
+                         {
+                             Id = roulettes.Id,
+                             Name = roulettes.Name,
+                             Items = grouping
+                         };
+
+            if(records.Count() > 0)
+            {
+                return Results.Ok(records.First());
+            } else
+            {
+                return Results.NotFound();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IResult> Create([FromBody] Roulette roulette)
+        {
+            database.Add(roulette);
+            await database.SaveChangesAsync();
+
+            return Results.Created();
+        }
+    }
+}
--- RouletteApi/Program.cs
+++ RouletteApi/Program.cs
@@ -5,41 +5,13 @@ using System.Reflection.Metadata;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DatabaseContext>();
+builder.Services.AddControllers();
 builder.Logging.AddSimpleConsole(c => c.SingleLine = true);
 
 var app = builder.Build();
-app.MapGet("/", (DatabaseContext database) =>
-{
-    var record = from roulette in database.Set<Roulette>()
-                 join item in database.Set<Item>()
-                 on roulette.Id equals item.RouletteId into grouping
-                 select new { 
-                     Id = roulette.Id, 
-                     Name = roulette.Name, 
-                     Items = from item in grouping 
-                             select new { 
-                                 Id = item.Id,
-                                 Name = item.Name,
-                                 Value = item.Value
-                             } 
-                 };
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
-    return Results.Ok(record);
-});
-app.MapPost("/", (DatabaseContext database) =>
-{
-    database.Add(new Roulette
-    {
-        Id = 1,
-        Name = "Podstawowa",
-        Items = [
-            new Item { Id = 1, Name = "Itemek 1", Value = 2 },
-            new Item { Id = 2, Name = "Itemek 2", Value = 8 },
-            new Item { Id = 3, Name = "Itemek 3", Value = 5 }
-        ]
-    });
-    database.SaveChanges();
-
-    return Results.Ok();
-});
 app.Run();
